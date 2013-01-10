914ce65b65004ef0321ccd4ac606303baaa47e36
Felix Gaehtgens <felix.gaehtgens@gmail.com>
2013-01-10 19:22:22 +0100

    Cleanup

--- EzAz-Java/src/org/EzAz/examples/simpleRequest/xmlRequest1.java
+++ EzAz-Java/src/org/EzAz/examples/simpleRequest/xmlRequest1.java
@@ -20,7 +20,6 @@ import java.io.InputStream;
 import java.util.Properties;
 
 import org.EzAz.Layer2.AttributeEntity;
-import org.EzAz.Layer2.Identifier;
 import org.EzAz.Layer2.Request;
 import org.EzAz.Layer2.Response;
 import org.EzAz.Layer3.PDPService;
@@ -30,61 +29,63 @@ import org.EzAz.generic.Layer2.genericResponse;
 
 public class xmlRequest1 {
 
-	public xmlRequest1() {
-		
+    public xmlRequest1() {
+
+    }
+
+    public void loadProps() {
+	Properties prop = new Properties();
+	// Load properties
+	InputStream propFile = this.getClass().getClassLoader()
+		.getResourceAsStream("org/EzAz/examples/simpleRequest/xmlRequest.properties");
+	try {
+	    prop.load(propFile);
+	    PDPserviceFactory.initSDK(prop);
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
-	
-	public void loadProps() {
-		Properties prop=new Properties();
-		// Load properties
-		InputStream propFile = this.getClass().getClassLoader().getResourceAsStream("org/EzAz/examples/simpleRequest/xmlRequest.properties");
-        try {
-        	prop.load(propFile);
-        	PDPserviceFactory.initSDK(prop);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	xmlRequest1 r = new xmlRequest1();
+	// Load properties file
+	r.loadProps();
+	// Initialize connection to PDP (policy decision point)
+	// called "dummy" and get a handle. This name is defined in
+	// the properties file.
+	// We need this handle to communicate with the PDP.
+	PDPService pdp = PDPserviceFactory.getPDP("dummy");
+	// Now construct a new request.
+	Request req = PDPserviceFactory.newRequest();
+
+	// Now create attributes on the request.
+	// h.addAttribute(AttributeEntity.CAT_SUBJECT,"subject-id", null,
+	// "jack");
+
+	req.addAttribute(AttributeEntity.CAT_ACTION, "urn:oasis:names:tc:xacml:1.0:action:action-id", null, "add")
+		.setIncludeInResult(true);
+	req.addAttribute(AttributeEntity.CAT_RESOURCE, "var1", null, 26).setIncludeInResult(true);
+	req.addAttribute(AttributeEntity.CAT_RESOURCE, "var2", null, 20).setIncludeInResult(true);
+	req.addAttribute(AttributeEntity.CAT_SUBJECT, "urn:org:apache:tomcat:user-role", null, "mathematician")
+		.setIncludeInResult(true);
+
+	// Print out the request
+	genericRequest.prettyPrint(System.out, "", req);
+
+	// Now call the PDP using the evaluate() function.
+	Response resp;
+	try {
+	    resp = pdp.evaluate(req);
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    return;
 	}
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		xmlRequest1 r=new xmlRequest1();
-		// Load properties file
-		r.loadProps();
-		// Initialize connection to PDP (policy decision point)
-		// called "dummy" and get a handle. This name is defined in
-		// the properties file.
-		// We need this handle to communicate with the PDP.
-		PDPService pdp=PDPserviceFactory.getPDP("dummy");
-		// Now construct a new request.
-		Request req=PDPserviceFactory.newRequest();
-
-
-		// Now create attributes on the request.
-		//h.addAttribute(AttributeEntity.CAT_SUBJECT,"subject-id", null, "jack");
-
-		req.addAttribute(AttributeEntity.CAT_ACTION, "urn:oasis:names:tc:xacml:1.0:action:action-id", null, "add").setIncludeInResult(true);
-		req.addAttribute(AttributeEntity.CAT_RESOURCE, "var1", null, 26).setIncludeInResult(true);
-		req.addAttribute(AttributeEntity.CAT_RESOURCE, "var2", null, 20).setIncludeInResult(true);
-		req.addAttribute(AttributeEntity.CAT_SUBJECT, "urn:org:apache:tomcat:user-role", null, "mathematician").setIncludeInResult(true);
-		
-		// Print out the request
-		genericRequest.prettyPrint(System.out, "", req);
-
-		// Now call the PDP using the evaluate() function.
-		Response resp;
-		try {
-			resp = pdp.evaluate(req);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
-		}
-		genericResponse.prettyPrint(System.out, "", resp);
-		// Check whether the response allows our request.
-		System.out.println("IS ALLOWED? "+resp.isAllowed(false));
-	}
-	
+	genericResponse.prettyPrint(System.out, "", resp);
+	// Check whether the response allows our request.
+	System.out.println("IS ALLOWED? " + resp.isAllowed(false));
+    }
 
 }
