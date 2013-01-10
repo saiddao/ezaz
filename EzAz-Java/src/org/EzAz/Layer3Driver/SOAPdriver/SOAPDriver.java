16f1dd5351129094297e98904bd8f4008a3f8f59
Felix Gaehtgens <felix.gaehtgens@gmail.com>
2013-01-10 19:10:40 +0100

    Changed copyright and error messages

--- EzAz-Java/src/org/EzAz/Layer3Driver/SOAPdriver/SOAPDriver.java
+++ EzAz-Java/src/org/EzAz/Layer3Driver/SOAPdriver/SOAPDriver.java
@@ -1,9 +1,20 @@
 package org.EzAz.Layer3Driver.SOAPdriver;
-
+/**
+ * Copyright 2012-2013 Felix Gaehtgens
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import java.util.Properties;
 
 import org.EzAz.AdviceObligationHandlers.AdviceObligationHandler;
-import org.EzAz.Layer2.Identifier;
 import org.EzAz.Layer2.Request;
 import org.EzAz.Layer2.Response;
 import org.EzAz.Layer2.Result;
@@ -43,13 +54,13 @@ public class SOAPDriver extends AsyncEmulator {
 	@Override
 	public Response extendedOperation(String operation, Request request,
 			Object extendedContext) {
-		throw new genericLayer2RuntimeException("AxiomaticsDriver does not implement extendedOperation!");
+		throw new genericLayer2RuntimeException("SOAPDriver does not implement extendedOperation!");
 	}
 
 	@Override
 	public Response extendedOperation(String operation, Request request,
 			Object extendedContext, Object appContext) {
-		throw new genericLayer2RuntimeException("AxiomaticsDriver does not implement extendedOperation!");
+		throw new genericLayer2RuntimeException("SOAPDriver does not implement extendedOperation!");
 	}
 
 	@Override
@@ -64,7 +75,7 @@ public class SOAPDriver extends AsyncEmulator {
 			Object extendedContext, Object appContext,
 			abstractSet<AdviceObligationHandler> adviceHandlers,
 			abstractSet<AdviceObligationHandler> obligationHandlers) {
-		throw new genericLayer2RuntimeException("AxiomaticsDriver does not implement extendedOperation!");
+		throw new genericLayer2RuntimeException("SOAPDriver does not implement extendedOperation!");
 	}
 
 	@Override
