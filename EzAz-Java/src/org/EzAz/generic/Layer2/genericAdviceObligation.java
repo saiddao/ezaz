3a25eed09dc0f8369b17d220eec0896bdf7d925f
Felix Gaehtgens <felix.gaehtgens@gmail.com>
2013-01-10 19:18:01 +0100

    attributeAssignments needs to be initialized, otherwise this can cause Null Pointer exceptions.

--- EzAz-Java/src/org/EzAz/generic/Layer2/genericAdviceObligation.java
+++ EzAz-Java/src/org/EzAz/generic/Layer2/genericAdviceObligation.java
@@ -1,17 +1,28 @@
 package org.EzAz.generic.Layer2;
-
+/**
+ * Copyright 2012-2013 Felix Gaehtgens
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import java.io.PrintStream;
 
 import org.EzAz.Layer2.AdviceObligation;
 import org.EzAz.Layer2.AdviceObligationSetter;
 import org.EzAz.Layer2.AttributeAssignment;
 import org.EzAz.Layer2.Identifier;
-import org.EzAz.Layer2.Response;
 import org.EzAz.Layer2.abstractSet;
 
 public class genericAdviceObligation implements AdviceObligation, AdviceObligationSetter {
 
-	private abstractSet<AttributeAssignment> attributeAssignments;
+	private abstractSet<AttributeAssignment> attributeAssignments=new abstractSet<AttributeAssignment>();
 	Identifier id;
 	int hasBeenHandled=0;
 	
