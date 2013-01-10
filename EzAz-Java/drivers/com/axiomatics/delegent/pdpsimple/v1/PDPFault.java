753d9c2cb235b14d471af0ef4aae49b27c04f558
Felix Gaehtgens <felix.gaehtgens@gmail.com>
2013-01-10 18:48:07 +0100

    Added version UID for serialization

--- EzAz-Java/drivers/com/axiomatics/delegent/pdpsimple/v1/PDPFault.java
+++ EzAz-Java/drivers/com/axiomatics/delegent/pdpsimple/v1/PDPFault.java
@@ -16,6 +16,10 @@ public class PDPFault
 {
 
     /**
+     * 
+     */
+    private static final long serialVersionUID = -6898224992460255386L;
+    /**
      * Java type that goes as soapenv:Fault detail element.
      * 
      */
