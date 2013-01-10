0bdf4d0d4eebda3cabb3d26459078847b38edfc6
Felix Gaehtgens <felix.gaehtgens@gmail.com>
2013-01-10 18:48:58 +0100

    Added copyright removed unneeded function sendXmlRequest

--- EzAz-Java/drivers/org/EzAz/Layer3Driver/XmlDriver/XmlDriver.java
+++ EzAz-Java/drivers/org/EzAz/Layer3Driver/XmlDriver/XmlDriver.java
@@ -1,5 +1,17 @@
 package org.EzAz.Layer3Driver.XmlDriver;
-
+/**
+ * Copyright 2012-2013 Felix Gaehtgens
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import java.io.InputStream;
 import java.util.List;
 import java.util.Properties;
@@ -114,10 +126,6 @@ public class XmlDriver extends AsyncEmulator {
 			req.getAttributes().add(atList);
 		}
 		return req;
-	}
-
-	private ResponseType sendXmlRequest(RequestType xmlReq) throws Exception {
-		throw new Exception("Not yet implemented!");
 	}
 
 	private org.EzAz.Layer2.Response generateEzAzResponse(ResponseType xmlResp) {
