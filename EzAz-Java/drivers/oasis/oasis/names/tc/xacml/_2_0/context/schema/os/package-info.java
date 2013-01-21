87472a65ca33afce9b47c40d18004292ed8de1c6
Felix Gaehtgens <felix.gaehtgens@gmail.com>
2013-01-20 22:28:57 -1000

    Files were in wrong directory - removed.

--- EzAz-Java/drivers/oasis/oasis/names/tc/xacml/_2_0/context/schema/os/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:oasis:names:tc:xacml:2.0:context:schema:os", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package oasis.names.tc.xacml._2_0.context.schema.os;
--- EzAz-Java/drivers/oasis/oasis/names/tc/xacml/_3_0/core/schema/wd_17/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:oasis:names:tc:xacml:3.0:core:schema:wd-17", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package oasis.names.tc.xacml._3_0.core.schema.wd_17;
